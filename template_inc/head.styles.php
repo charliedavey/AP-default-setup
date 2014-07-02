ebf7577556f65deacee095619dee68db26b3ee5c
charliedavey <Charlie@Charlies-MacBook-Pro-2.local>
2014-07-02 15:25:18 +0100

    add plugins.css

--- template_inc/head.styles.php
+++ template_inc/head.styles.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="/css/custom.css?v=1.0">
+<link rel="stylesheet" href="/css/plugins.css?v=1.0">
 <link rel="stylesheet" href="/css/print.css" media="print">
 <script src="/js/modernizr.2.8.2.js"></script>
